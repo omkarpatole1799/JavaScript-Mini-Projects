690bd3bd170e8c768e676d7cbb3ddb080eb54583
Omkar Patole <omkarpatole1799@gmail.com>
2023-01-28 14:39:54 +0530

    added rules and health

--- Dice-game/script3.js
+++ Dice-game/script3.js
@@ -10,6 +10,9 @@ const p1CurrentScore = document.querySelector("#current--1")
 const player0 = document.querySelector(".player--0")
 const player1 = document.querySelector(".player--1")
 
+// life
+const lifeElement = document.querySelector(".life")
+
 // selecting buttons
 const newGame = document.querySelector('.btn--new')
 const rollDice = document.querySelector('.btn--roll')
@@ -19,9 +22,11 @@ const holdBtn = document.querySelector('.btn--hold')
 let diceImage = document.querySelector('.dice')
 
 // state variables
-let currentScore, currentPlayer, scores, isPlaying
+let currentScore, currentPlayer, scores, isPlaying, isRolled, life
 
 function initial() {
+    life = 3
+    isRolled = false
     isPlaying = true
     currentPlayer = 0
     currentScore = 0
@@ -38,7 +43,7 @@ function initial() {
 
     player0.classList.remove('player--winner')
     player1.classList.remove('player--winner')
-    
+    lifeElement.textContent = "Health: " + life
 }
 initial()
 
@@ -48,15 +53,18 @@ function switchPlayer() {
     currentPlayer = currentPlayer === 0 ? 1 : 0
     player0.classList.toggle('player--active')
     player1.classList.toggle('player--active')
+    life = 3
+    lifeElement.textContent = "Health: " + life
 }
 
 // rolling dice
 rollDice.addEventListener("click", function () {
 
     if (isPlaying) {
+        isRolled = true
+
         // generate random number between 1-6
         let diceNum = Math.trunc(Math.random() * 6) + 1
-        console.log(diceNum)
 
         // set dice according to number generated
         diceImage.classList.remove('hidden')
@@ -64,25 +72,30 @@ rollDice.addEventListener("click", function () {
 
         // if diceNum is 1 then reset score to zero and switch player
 
-        if (diceNum !== 1) {
+        if (diceNum !== 1 && life !== 0) {
             // add score to current score
             currentScore += diceNum
             document.querySelector(`#current--${currentPlayer}`)
                 .textContent = currentScore
 
+            life--
+            lifeElement.textContent = "Health: " + life
+
         } else {
             // switch player
+            scores[currentPlayer] += currentScore
+            document.getElementById(`score--${currentPlayer}`).textContent = scores[currentPlayer]
             switchPlayer()
         }
     }
 })
 
 // new game button
-newGame.addEventListener("click",initial)
+newGame.addEventListener("click", initial)
 
 // hold button switch player
-holdBtn.addEventListener("click", function () {
 
+holdBtn.addEventListener("click", function () {
     if (isPlaying) {
         scores[currentPlayer] += currentScore
         document.getElementById(`score--${currentPlayer}`)
@@ -100,3 +113,32 @@ holdBtn.addEventListener("click", function () {
         }
     }
 })
+
+// model js 
+
+const rulesButton = document.querySelector('.btn--rules')
+const closeModelBtn = document.querySelector(".close-modal")
+const model = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+// show model function
+function showModel() {
+    model.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+function hideModel() {
+    model.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const btnArr = [closeModelBtn, overlay]
+
+rulesButton.addEventListener("click", function () {
+    showModel()
+})
+
+for (let i = 0; i < btnArr.length; i++) {
+    btnArr[i].addEventListener("click", function () {
+        hideModel()
+    })
+}
