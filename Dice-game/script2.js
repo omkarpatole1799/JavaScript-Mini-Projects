8c83b74898a487d0fbde0cc1d5260624457ad0f9
Omkar Patole <omkarpatole1799@gmail.com>
2023-01-28 08:41:38 +0530

    dice game

---
+++ Dice-game/script2.js
@@ -0,0 +1,104 @@
+'use strict'
+
+// selecting final scores of players
+const score0Element = document.getElementById('score--0')
+const score1Element = document.getElementById('score--1')
+
+// selecting current scores of players
+const current0Element = document.getElementById('current--0')
+const current1Element = document.getElementById('current--1')
+
+// selecting players
+const player0 = document.querySelector('.player--0')
+const player1 = document.querySelector('.player--1')
+
+// selecting the buttons
+const newGameBtn = document.querySelector('.btn--new')
+const rollDiceBtn = document.querySelector('.btn--roll')
+const holdGameBtn = document.querySelector('.btn--hold')
+
+let diceImage = document.querySelector('.dice')
+
+let scores, currentScore, currentPlayer
+// initial condition 
+
+function initial() {
+    // scores of players in array
+    scores = [0, 0]
+    currentScore = 0
+    currentPlayer = 0
+
+    score0Element.textContent = 0
+    score1Element.textContent = 0
+
+    current0Element.textContent = 0
+    current1Element.textContent = 0
+
+    // hiding the dice before starting the game
+    diceImage.classList.add('hidden')
+
+    // switching to player 0 
+    player0.classList.add('player--active')
+    player1.classList.remove('player--active')
+    
+}
+initial();
+
+// switch player function
+function switchPlayer(){
+    // switch player
+    document.getElementById(`current--${currentPlayer}`)
+        .textContent = 0
+    currentScore = 0
+    currentPlayer = currentPlayer === 0 ? 1 : 0
+
+    // changing active class of corresponding player
+    player0.classList.toggle('player--active')
+    player1.classList.toggle('player--active')
+}
+
+// rolling the dice when button pressed
+rollDiceBtn.addEventListener("click",function(){
+
+    // generate random number between 1-6
+    let diceRollNumber = Math.trunc(Math.random() * 6) + 1
+
+    // setting dice according to rolled dice
+    diceImage.classList.remove('hidden')
+    diceImage.src = `dice-${diceRollNumber}.png`
+
+    // if the score is 1 then make score 0 and switch player
+    if(diceRollNumber !== 1){
+        // add score to corresponding player
+        currentScore += diceRollNumber
+        document.getElementById(`current--${currentPlayer}`)
+        .textContent = currentScore
+    } else {
+        // switch player
+        switchPlayer()
+    } 
+})
+
+// new Game button 
+newGameBtn.addEventListener("click",initial)
+
+// hold score
+holdGameBtn.addEventListener("click",function(){
+
+    // if(currentPlayer === 0){
+    //     scores[currentPlayer] += currentScore
+    //     score0Element.textContent = scores[currentPlayer]
+    //     switchPlayer()
+    // } else if (currentPlayer === 1){
+    //     scores[currentPlayer] += currentScore
+    //     score1Element.textContent = scores[currentPlayer]
+    //     switchPlayer()
+    // }    
+
+    scores[currentPlayer] += currentScore
+    
+    document.getElementById(`score--${currentPlayer}`)
+    .textContent = scores[currentPlayer]
+
+    switchPlayer()
+})
