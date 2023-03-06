aa9508f1d420494fe1526c8baea3ce3577299fba
Omkar Patole <omkarpatole1799@gmail.com>
2023-03-06 22:21:16 +0530

    todo app final

--- TO-DO/index.js
+++ TO-DO/index.js
@@ -1,38 +1,64 @@
 "use strict";
 console.log("Hi");
 
-const taskContainer = document.querySelector(".taskContainer");
-const submitBtn = document.querySelector('.submit')
-const task_heading = document.getElementById('task_heading')
-const task_description = document.getElementById('task_description')
+const todoContainer = document.querySelector(".todoContainer");
+const todoSubmit = document.querySelector('.todoSubmitBtn')
+const todoHeading = document.getElementById('task_heading')
+const todoDescription = document.getElementById('task_description')
 
 // data
-const todoList = [];
+const todoListArr = [];
+todoContainer.innerHTML = "Start adding your todo list!"
 
-submitBtn.addEventListener("click", function (e) {
+// add todo functionality
+todoSubmit.addEventListener("click", function (e) {
     e.preventDefault();
+    const todoHeadingValue = todoHeading.value;
+    const todoDescriptionValue = todoDescription.value;
 
-    // inserting the values in array of objects
-    todoList.push({
-        "heading": task_heading.value,
-        "description": task_description.value
-    })
-    displayTodoList(todoList)
+    // check if the input values are empty
+    if (todoHeadingValue !== "" && todoDescriptionValue !== "") {
+        // inserting the values in array of objects
+        todoListArr.push({
+            "heading": todoHeadingValue,
+            "description": todoDescriptionValue
+        })
+
+        displayTodoList(todoListArr)
+    } else {
+        alert("Insert information")
+    }
+    todoHeading.textContent = ''
+    todoDescription.textContent = ''
 })
 
+// display todo
 const displayTodoList = function (todoList) {
-    taskContainer.innerHTML = "";
+    todoContainer.innerHTML = "";
 
-    // loop through each item
-    todoList.forEach(function (el, i) {
-        const html = `<div class="card" style="width: 18rem;">
+    // check if array is empty or not
+    if (todoListArr.length !== 0) {
+        // loop through each item
+        todoList.forEach(function (el, i) {
+            const html = `<div class="card" style="width: 18rem;">
                     <div class="card-body">
                         
                         <h5 class="card-title">Task ${i + 1} : ${el.heading}</h5>
                         <p class="card-text">Description: ${el.description}</p>
+                        <button class="todoCompleteBtn btn btn-primary">Done</button>
                     </div>
                  </div>`;
-        taskContainer.insertAdjacentHTML("afterbegin", html);
-    });
-    return todoList
+            todoContainer.insertAdjacentHTML("afterbegin", html);
+
+            // complete the task
+            const todoCompleteBtn = document.querySelector('.todoCompleteBtn')
+            todoCompleteBtn.addEventListener("click", function (e) {
+                e.preventDefault()
+                todoListArr.splice(i, 1)
+                displayTodoList(todoListArr)
+            })
+        });
+    } else {
+        todoContainer.innerHTML = "Hurray! no task due"
+    }
 }
