da013dd526914d2213f4ceb1c80f6f1266c9874a
Omkar Patole <omkarpatole1799@gmail.com>
2023-03-06 21:20:46 +0530

    init

--- TO-DO/index.js
+++ TO-DO/index.js
@@ -7,41 +7,33 @@ const task_heading = document.getElementById('task_heading')
 const task_description = document.getElementById('task_description')
 // data
 const todoList = [
-    {
-        "heading": "test todo",
-        "description": "test description",
-    },
-
 
 ];
 
 submitBtn.addEventListener("click", function (e) {
     e.preventDefault();
 
-    console.log(typeof heading)
     todoList.push({
         "heading": task_heading.value,
         "description": task_description.value
     })
-    displayTodoList(todoList)
+
+    console.log(displayTodoList(todoList))
 })
 
-const displayTodoList = function (todoListFun) {
-    console.log(todoListFun)
-
-    todoListFun.forEach(function (el, i) {
-        const taskHeading = el.heading;
-        const taskDescription = el.description;
-        console.log(el);
-
+const displayTodoList = function (todoList) {
+    taskContainer.innerHTML = "";
+    todoList.forEach(function (el, i) {
 
         const html = `<div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${taskHeading}</h5>
-                        <p class="card-text">${taskDescription}</p>
+                        
+                        <h5 class="card-title">Task ${i + 1} : ${el.heading}</h5>
+                        <p class="card-text">Description: ${el.description}</p>
                     </div>
                  </div>`;
 
         taskContainer.insertAdjacentHTML("afterbegin", html);
     });
+    return todoList
 }
